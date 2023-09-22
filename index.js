1ccf36ff43e1b9e0d882356dc514f95b187376bf
mbari60 <kevinmbari600@gmail.com>
2023-09-22 16:15:28 +0300

    function continue lab done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(text){
+ if (text === "bathe my dog"){
+    return "This Saturday, I want to bathe my dog!";
+ } else {
+  return "This Saturday, I want to roller-skate!";
+}
+}
+
+function mondayWork(text){
+     if (text === "work from home"){
+       return "This Monday, I will work from home.";
+     } else {
+       return "This Monday, I will go to the office.";
+     }
+}
+function wrapAdjective(emphasis = "*") {
+  return function (adjective = "special") {
+    return `You are ${emphasis}${adjective}${emphasis}!`;
+  };
+}
+
+const result = wrapAdjective("*");
+const emphatic = result("a hard worker");
+console.log(emphatic);
